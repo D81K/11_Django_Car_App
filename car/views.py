b43cf45c12eb7a928f00fd8a2b90f0c15a5c21fb
dario.karmeinski <dario.karmeinski@gmail.com>
2023-01-12 22:38:05 +0100

    Adds modified user views

--- car/views.py
+++ car/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from .models import Car, Reservation
 from .serializers import CarSerializer, ReservationSerializer
-from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.viewsets import ModelViewSet
 
 
@@ -17,12 +16,27 @@ class CarListView(ListAPIView):
     # permission_classes = [IsAdminUser]
     # permission_classes = (IsAuthenticated, )
 
-class ReservationView(ListCreateAPIView):
-    queryset = Reservation.objects.all()
-    serializer_class = ReservationSerializer
-
-
 class AdminCarViewSet(ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
     permission_classes = (IsAdminUser, )
+
+class ReservationListCreateView(ListCreateAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = (IsAuthenticated, )
+
+class ReservationDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the reservations to staff members
+        and the reservations of the current user.
+        """
+        queryset = super().get_queryset()
+        if self.request.user.is_staff:
+            return queryset
+        return Reservation.objects.filter(customer = self.request.user)
